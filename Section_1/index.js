fa15d6d14cbfea3d825cdc0f9b986638a7de4600
AmitBloodLine <bloodline90265@gmail.com>
2024-02-16 18:29:52 +0530

    0216

---
+++ Section_1/index.js
@@ -0,0 +1,23 @@
+console.log('Welcome to JavaScript');
+
+a = 50;
+myname="Amit BloodLine";
+
+isHungry = true;
+
+empty = null;
+
+console.log(a);
+console.log(myname);
+
+console.log( typeof a );
+console.log( typeof(myname) );
+
+console.log( 1 + '1' );
+
+console.log( 1 == '1' );
+console.log( 1 === '1' );
+
+// exponent operator
+
+console.log( 4 ** 3 );
---
+++ Section_1/keyword.js
@@ -0,0 +1,10 @@
+if(34 % 2 === 0 ){
+    console.log('even');
+
+    abc = 'nice';
+
+}else{
+    console.log('odd');
+}
+
+console.log(abc);
