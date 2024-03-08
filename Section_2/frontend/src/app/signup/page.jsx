b2f65cf0225deef1d695bc7c715ef19f98cb54cb
AmitBloodLine <bloodline90265@gmail.com>
2024-03-08 16:07:55 +0530

    validationformbugfix

--- Section_2/frontend/src/app/signup/page.jsx
+++ Section_2/frontend/src/app/signup/page.jsx
@@ -1,37 +1,38 @@
-'use client'
+"use client";
 import React from "react";
 import { useFormik } from "formik";
-import * as Yup from 'yup';
+import * as Yup from "yup";
 
 const validationSchema = Yup.object({
-  username:Yup.string()
-    .required('Username is required'),
+  username: Yup.string().required("Username is required"),
 
-  email:Yup.string()
-  .email('Invalid email address')
-  .notRequired('Email is required'),
+  email: Yup.string()
+    .email("Invalid email address")
+    .required("Email is required"),
   password: Yup.string()
-  .min(8,'Password should be minimum 8 characters')
-  .required('Password is required'),
+    .min(8, "Password should be minimum 8 characters")
+    .required("Password is required"),
   confirmPassword: Yup.string()
-  .min(8,'Password should be minimum 8 characters')
-  .required('Password is required')
-
-})
+    .min(8, "Password should be minimum 8 characters")
+    .required("Password is required"),
+});
 const page = () => {
-
   const formik = useFormik({
-    initialValues :{
-      username : '',
-      email:'',
-      password:'',
-      confirmPassword:'',
+    initialValues: {
+      username: "",
+      email: "",
+      password: "",
+      confirmPassword: "",
     },
     validationSchema: validationSchema,
-    onSubmit : values =>{
-      console.log(values);
-    }
-  })
+    onSubmit: (values) => {
+      if (values.password === values.confirmPassword) {
+        console.log(values);
+      } else {
+        console.log("Password do not match");
+      }
+    },
+  });
 
   return (
     <div>
@@ -42,7 +43,7 @@ const page = () => {
               <h2 className="text-center">Brand Logo</h2>
               <h2 className="text-center">SignUp</h2>
 
-              <form>
+              <form onSubmit={formik.handleSubmit}>
                 <div class="mb-3">
                   <label for="username" class="form-label">
                     Username
@@ -58,11 +59,13 @@ const page = () => {
                     onChange={formik.handleChange}
                     value={formik.values.username}
                   />
-                  {formik.errors.username ? <div>{formik.errors.username}</div> : null}
+                  {formik.errors.username ? (
+                    <div>{formik.errors.username}</div>
+                  ) : null}
                 </div>
 
                 <div class="mb-3">
-                <label for="email" class="form-label">
+                  <label for="email" class="form-label">
                     Email Address
                   </label>
 
@@ -76,7 +79,9 @@ const page = () => {
                     onChange={formik.handleChange}
                     value={formik.values.email}
                   />
-                  {formik.errors.email ? <div>{formik.errors.email}</div> : null}
+                  {formik.errors.email ? (
+                    <div>{formik.errors.email}</div>
+                  ) : null}
                 </div>
 
                 <div class="mb-3">
@@ -94,7 +99,9 @@ const page = () => {
                     onChange={formik.handleChange}
                     value={formik.values.password}
                   />
-                  {formik.errors.password ? <div>{formik.errors.password}</div> : null}
+                  {formik.errors.password ? (
+                    <div>{formik.errors.password}</div>
+                  ) : null}
                 </div>
 
                 <div class="mb-3">
@@ -112,17 +119,26 @@ const page = () => {
                     onChange={formik.handleChange}
                     value={formik.values.confirmPassword}
                   />
-                  {formik.errors.confirmPassword ? <div>{formik.errors.confirmPassword}</div> : null}
+                  {formik.errors.confirmPassword ? (
+                    <div>{formik.errors.confirmPassword}</div>
+                  ) : null}
                 </div>
 
                 <div class="form-check">
-                  <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault" />
-                    <label class="form-check-label" for="flexCheckDefault">
-                      Remember Me
-                    </label>
+                  <input
+                    class="form-check-input"
+                    type="checkbox"
+                    value=""
+                    id="flexCheckDefault"
+                  />
+                  <label class="form-check-label" for="flexCheckDefault">
+                    Remember Me
+                  </label>
                 </div>
 
-                <button type="submit" class="btn btn-dark w-100 my-4">Register</button>
+                <button type="submit" class="btn btn-dark w-100 my-4">
+                  Register
+                </button>
               </form>
             </div>
           </div>
